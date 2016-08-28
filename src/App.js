1584f9fa84eff48ed50658c271e4592dd2c749af
Kris Vukasinovic <axellerate@hotmail.com>
2016-08-28 18:53:39 -0400

    added callback to sections for click methods

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import NaturePort from './components/NaturePort';
 import Section from './components/Section';
+import $ from 'jquery'; 
 
 class App extends Component {
 
+	dissolve() {
+		var swap_direction = ["left", "right"];
+		for(var i = 0; i<2; i++) {
+			$("#" + Number(i)).hide();
+		}
+	}
+
 	menuButtonClicked(action) {
-		alert(action);
+		this.dissolve();
+		console.log(action);
 	}
 
   render() {
     return (
       <div>
-        <Header />
-        <NaturePort />
-        <Section />
+        <Header clickAction={ this.menuButtonClicked.bind(this) }/>
+        <NaturePort id="0"/>
+        <Section id="1"/>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -65,18 +65,37 @@ class Header extends Component {
     });
 	}
 
+	buttonClicked(action) {
+		this.props.clickAction(action);
+	}
+
   render() {
     return (
       <header style={ headerStyle }>
         <img style={ logoStyle } src={ LogoImage } alt="Habit@ Logo" role="presentation"/>
       	<menu style={ mainMenuStyle }>
-					<span style={ theWorldButtonStyle } id="login-button">The World</span>
-      		<span style={ statsButtonStyle } id="stats-button">Stats</span>
-      		<span style={ learnMoreButtonStyle } id="login-button">Learn More</span>
+					<span style={ theWorldButtonStyle } id="login-button" 
+						onClick={ () => {this.buttonClicked("/the-world")} }>
+						The World
+					</span>
+      		<span style={ statsButtonStyle } id="stats-button"
+      			onClick={ () => {this.buttonClicked("/stats")} }>
+      			Stats
+      		</span>
+      		<span style={ learnMoreButtonStyle } id="learn-more-button"
+      			onClick={ () => {this.buttonClicked("/learn-more")} }>
+      			Learn More
+      		</span>
       	</menu>
       	<menu style={ loginRegisterMenuStyle }>
-      		<span style={ loginButtonStyle } id="login-button">Login</span>
-      		<span style={ registerButtonStyle } id="register-button">Register</span>
+      		<span style={ loginButtonStyle } id="login-button"
+      			onClick={ () => {this.buttonClicked("/login")} }>
+      			Login
+      		</span>
+      		<span style={ registerButtonStyle } id="register-button"
+      			onClick={ () => {this.buttonClicked("/register")} }>
+      			Register
+      		</span>
       	</menu>
       </header>
     );
